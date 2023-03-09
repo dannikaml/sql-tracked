259a8de6052e1b4c7825c8b5da1b68a649bf6584
dannikaml <dannikaml@hotmail.com>
2023-03-08 20:31:31 -0600

    add connect to database; add import and require mysql2

---
+++ db/conntection.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+{
+    host: "localhost",
+    user: "root",
+    password: "panthers123",
+    database: "employees_db"
+},
+console.log(`Connected to the employees_db database...`)
+);
+
+connection.connect(function (err) {
+if (err) throw err;
+}
+);
+
+module.exports = db;
